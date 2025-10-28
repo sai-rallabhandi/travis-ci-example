71d55e1c7ea41262e94fb078adde8cb5b5eb35b2
rallabhandi0201 <rallabhandi0201@gmail.com>
2025-10-28 17:02:34 +0000

    updated the project files

---
+++ src/main/java/com/travis/travis_ci_example/TravisCiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.travis.travis_ci_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travis/travis_ci_example/TravisCiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travis.travis_ci_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
