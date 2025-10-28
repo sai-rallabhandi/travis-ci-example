a5a5b8e02dc61bc2bfa763b083a24b42e6b1f5a3
sai-rallabhandi <sai.rallabhandi0201@gmail.com>
2025-10-28 20:22:01 +0000

    Update TravisCiExampleApplication.java

--- src/main/java/com/travis/travis_ci_example/TravisCiExampleApplication.java
+++ src/main/java/com/travis/travis_ci_example/TravisCiExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravisCiExampleApplication {
 
 	public static void main(String[] args) {
+		//this is a comment
 		SpringApplication.run(TravisCiExampleApplication.class, args);
 	}
 
